5cfc2d46de0913bca284e00c88e280957550c186
robanc <robanc@yahoo.com>
2026-01-22 22:28:12 +0000

    pipeline code working

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -30,18 +31,33 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
+# def run():
+#    pg_user = 'root'
+#    pg_pass = 'root'
+#    pg_host = 'localhost'
+#    pg_port = 5432
+#    pg_db = 'ny_taxi'
+#
+#    year = 2021
+#    month = 1
+#
+#    target_table = 'yellow_taxi_data'
+#    chunksize = 100000
 
-    year = 2021
-    month = 1
+@click.command()
+@click.option('--pg_user', default='root', help='PostgreSQL user')
+@click.option('--pg_pass', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='PostgreSQL year of the data')
+@click.option('--month', default=1, type=int, help='PostgreSQL year of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunksize for igestion')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
 
-    target_table = 'yellow_taxi_data'
-    chunksize = 100000
+
+
 
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
